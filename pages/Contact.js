3ad0cf2ccc5d219757b2c2011b5ba4f90fb7b53e
murdersnakes <92579054+murdersnakes@users.noreply.github.com>
2022-04-02 11:22:56 +0100

    build 1

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,14 +1,8 @@
 import Layout from "../components/Layout";
 import Head from "next/dist/shared/lib/head";
 import styles from '../styles/contact.module.scss';
-import { useState } from "react/cjs/react.development";
 
 export default function Contact() {
-
-    const [fullname, setFullname] = useState("");
-    const [email, setEmail] = useState("");
-    const [company, setCompany] = useState("");
-    const [message, setMessage] = useState("");
 
     return (
         <>
@@ -20,47 +14,8 @@ export default function Contact() {
             <Layout>
                 <div className={styles.container}>
                     <div className={styles.card}>
-                        <form>
-                            <h1>Send us a message</h1>
-
-                            <label htmlFor="fullname">Full name</label>
-                            <input type="text" value={fullname}
-                                onChange={(e) => {
-                                    setFullname(e.target.value);
-                                }}
-                                name="fullname"
-                            />
 
 
-                            <label htmlFor="email">Email address</label>
-                            <input
-                                type="email"
-                                name="email"
-                                value={email}
-                                onChange={(e) => {
-                                    setEmail(e.target.value);
-                                }}></input>
-
-                            <label htmlFor="company">Company</label>
-                            <input
-                                name="company"
-                                value={company}
-                                onChange={(e) => {
-                                    setCompany(e.target.value);
-                                }}
-                            ></input>
-
-                            <label htmlFor="message">Message</label>
-                            <textarea
-                                name="message"
-                                value={message}
-                                onChange={(e) => {
-                                    setMessage(e.target.value);
-                                }}
-                            ></textarea>
-
-                            <input type="submit" name="button">Submit</input>
-                        </form>
                     </div>
                 </div>
             </Layout>
